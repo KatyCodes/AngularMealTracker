b9cf581029f7f0da3854c1f9a535e6e09d7b34a1
Katy Henning <katyannhenning@gmail.com>
2016-10-16 15:33:39 -0700

    adds template

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+  <div class="jumbotron">
+    <h1>My First Angular 2 App</h1>
+  </div>
+  `
+})
+
+export class AppComponent {
+
+}
